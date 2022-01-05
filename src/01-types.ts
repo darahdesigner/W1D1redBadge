dcec37c3f1b93f7fa676a7979aadc84c4744946a
Darah Snider <33365227+darahdesigner@users.noreply.github.com>
2022-01-05 18:47:00 -0500

    initial commit, ts types number, string, boolean

---
+++ src/01-types.ts
@@ -0,0 +1,38 @@
+//Type Declarations
+
+//Will not work
+// let x = 7;
+// x = "seven";
+
+//Declare types w/out initialization
+let address: string;
+let phoneNumber: string;
+let email : string;
+let sS: number;
+let isMarried: boolean;
+
+//Declare types with initialization
+let username : string = "instructor";
+let password : string = "password"
+let instructorId : number = 1;
+
+let greeting = "hello";
+
+//Basic types
+//Primitives
+
+//* Number
+let age: number = 40;
+let currentYear : number = 2022;
+let balance : number = 1_000_000;
+//*string
+let companyName: string;
+companyName= "EFA";
+let appName : string;
+appName = "twitter";
+//*Boolean
+let isLoggedIn: boolean=true;
+let isAdmin: boolean = true;
+
+//*Arrays
+
